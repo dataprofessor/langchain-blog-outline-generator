5d9d865a448a2aefafdb5a2dd8e14c633f16c4d5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-17 20:06:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,23 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain import PromptTemplate
 
-st.title('🦜🔗 Langchain - Basic App')
+st.title('🦜🔗 Langchain - Blog Outline Generator App')
 
-llm = OpenAI(model_name='text-davinci-003')
+def blog_outline(topic)
+  # Instantiate LLM model
+  llm = OpenAI(model_name='text-davinci-003')
+  # Prompt
+  template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
+  prompt = PromptTemplate(input_variables = ['topic'], template = template)
+  prompt_query = prompt.format(topic=topic)
+  # Run LLM model
+  response = llm(prompt_query)
+  # Print results
+  return st.info(response)
 
 with st.form:
-  text_input = st.text_input('Enter your prompt:', '')
+  topic_text = st.text_input('Enter prompt:', '')
   submitted = st.form_submit_button('Submit')
   if submitted:
-    llm(text_input)
+    blog_outline(topic_text)
