e8e0de2864cbc6fd45ceaf67e4ee3ccf02260094
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-07 06:40:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate
 
 st.set_page_config(page_title="🦜🔗 Blog Outline Generator App")
-
 st.title('🦜🔗 Blog Outline Generator App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
